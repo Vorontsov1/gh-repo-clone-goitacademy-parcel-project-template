dc979ab54421f0176ec9532a79222d177fcc92bf
Vorontsov1 <101946287+Vorontsov1@users.noreply.github.com>
2022-09-21 19:36:26 +0000

    Deploying to gh-pages from @ Vorontsov1/goit-js-hw-09@28c1edc09b2f1ea2b7709fdfbe48a192ba5d23f1 🚀

---
+++ 01-color-switcher.5f5bc9c9.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");e.addEventListener("click",onColorSwitcher),t.addEventListener("click",offColorSwitcher);
+//# sourceMappingURL=01-color-switcher.5f5bc9c9.js.map
