28c1edc09b2f1ea2b7709fdfbe48a192ba5d23f1
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-21 22:35:58 +0300

    started_do_hw_09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+startBtn.addEventListener('click', onColorSwitcher);
+stopBtn.addEventListener('click', offColorSwitcher);
+
 
