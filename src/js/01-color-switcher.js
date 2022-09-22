54131f18611b709f788a41b3805e6f3da16345e9
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-22 22:57:32 +0300

    finished_color_switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,27 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 
 let timerId = null;
+
 startBtn.addEventListener('click', onColorSwitcher);
 stopBtn.addEventListener('click', offColorSwitcher);
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function createColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onColorSwitcher() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerId = setInterval(createColor, 1000);
+}
+
+function offColorSwitcher() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+}
 
