934db8a97e8505cd08bfcf5617fa5f257b1a6124
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-09-24 22:05:26 +0300

    Update_09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,8 @@
 import Notiflix from 'notiflix';
 
+const form = document.querySelector('.form');
+form.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
