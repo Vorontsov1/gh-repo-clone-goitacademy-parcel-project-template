7a8dc5cf40fb996d833ea096090636cea9321333
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-04 20:04:50 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './css/styles.css';
+import { debounce  } from 'lodash';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './js/fetchCountries';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+countryInput.addEventListener('input', debounce(onCountryInput.DEBOUNCE_DELAY))
+
+function onCountryInput() {
+    const name = countryInput.value.trim()
+    if (name === '') {
+        return(countryList.innerHTML = ''),(countryInfo.innerHTML = '')
+    }
+}
