5041029ef236532274e1418cf9a6fa26346c42e5
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-05 20:55:20 +0300

    going_task_10_js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
 import './css/styles.css';
-import { debounce  } from 'lodash';
-import Notiflix from 'notiflix';
-import { fetchCountries } from './js/fetchCountries';
 
+import { fetchCountries } from './js/fetchCountries';
+import {
+  buildCountry,
+  buildCountryList,
+  countryInfo,
+  countryList,
+  clearSearchResult,
+} from './js/countries';
+
+
+import Notiflix, { Notify } from 'notiflix';
+import { debounce } from 'lodash.debounce';
+
+Notiflix.Notify.init({
+  width: '300px',
+  position: 'right-top',
+  timeout: 2000,
+});
 
 const DEBOUNCE_DELAY = 300;
 
-const countryInput = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+const searchBox = document.querySelector('#search-box');
 
-countryInput.addEventListener('input', debounce(onCountryInput.DEBOUNCE_DELAY))
+searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
-function onCountryInput() {
-    const name = countryInput.value.trim()
-    if (name === '') {
-        return(countryList.innerHTML = ''),(countryInfo.innerHTML = '')
+function onSearch() {
+    const searchName = searchBox.ariaValueMax.trim().toLowerCase();
+    console.log(searchName);
+    if (searchName) {
+        fetchCountries(searchName)
+            .then(resultSearch)
+            .catch(error => {
+                Notify.failure('Oops, there is no counrty with that name');
+                clearSearchResult();
+            });
+    } else if (searchName === 0) {
+        clearSearchResult();
+    }
+}
+
+function resultSearch(country) {
+    if (counrty.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        clearSearchResult();
+        return;
+    } else if (counrty.length === 1) {
+        buildCountryList(country);
+        countryList.innerHTML = '';
+        return;
+    } else {
+        buildCountry(country);
+        countryInfo.innerHTML = '';
+        return;
     }
 }
