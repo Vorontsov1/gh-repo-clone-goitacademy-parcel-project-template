d58088f4e7061afaf75280f33ae142c5be55c884
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-09 23:09:31 +0300

    going_more

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import { fetchImages } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
-
-
-// Описан в документации
+import { onScroll, onTopbtn } from './js/scroll';
 import SimpleLightbox from "simplelightbox";
-// Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+
+searchForm.addEventListener('submit', onSearchForm);
+loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+
+onScroll();
+onTopbtn();
+
+function onSearchForm(e) {
+    e.preventDefault();
+}
+
+
+function onLoadMoreBtn() {
+    
+}
