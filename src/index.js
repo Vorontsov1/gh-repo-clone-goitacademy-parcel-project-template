83112efbd01d1c839c34de0d503b511c965b7751
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-10-11 21:59:57 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,27 +1,93 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
-import { fetchImages } from './js/fetch-images';
+import { fetchImg } from './js/fetch-images';
 import { renderGallery } from './js/render-gallery';
-import { onScroll, onTopbtn } from './js/scroll';
+import { onScroll, onTopBtn } from './js/scroll';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import simpleLightbox from 'simplelightbox';
 
 const searchForm = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+const loadMore = document.querySelector('.load-more');
+let query = '';
+let page = 1;
+let simpleLightBox;
+let perPage = 40;
 
 
 searchForm.addEventListener('submit', onSearchForm);
-loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+loadMore.addEventListener('click', onLoadMore);
 
 onScroll();
-onTopbtn();
+onTopBtn();
 
 function onSearchForm(e) {
     e.preventDefault();
+    window.scrollTo({ top: 0 });
+    page = 1;
+    query = e.currentTarget.searchQuery.value.trim();
+    gallery.innerHTML = '';
+    loadMore.classList.add('is-hidden');
+
+    if (query === '') {
+        emptySearch();
+        return;
+    }
+
+    fetchImg(query, page, perPage).then(({ data }) => {
+        if (data.totalHits === 0) {
+            imagesNotFound();
+        } else {
+            renderGallery(data.hits);
+            simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+            imagesFound(data);
+
+            if (data.totalHits > perPage) {
+                loadMore.classList.remove('is-hidden');
+            }
+        }
+    })
+        .catch(error => console.log(error))
+        .finally(() => {
+        searchForm.reset();
+    });
 }
 
 
-function onLoadMoreBtn() {
-    
+function onLoadMore() {
+    page += 1;
+    simpleLightbox.destroy();
+
+    fetchImg(query, page, perPage)
+        .then(({ data }) => {
+            renderGallery(data.hits);
+            simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+            const totalPages = Math.ceil(data.totalHits / perPage);
+
+            if (page > totalPages) {
+                loadMore.classList.add('is-hidden');
+                endOfSearch();
+            }
+        })
+        .catch(error => console.log(error));
+}
+
+function emptySearch() {
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.');
+}
+
+function imagesNotFound() {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.');
+}
+
+function imagesFound(data) {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+}
+
+function endOfSearch() {
+    Notiflix.Notify.failure(
+      "We're sorry, but you've reached the end of search results.");
 }
--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+export { fetchImg };
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const KEY = '24543353-3824dfbf23e7b5ead533e5f72';
+
+async function fetchImg(query, page, perPage) {
+  const response = await axios.get(
+    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+  );
+  return response;
+}
